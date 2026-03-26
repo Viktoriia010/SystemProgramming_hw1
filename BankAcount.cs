1edf4be1c07be8ac70c20298cd380f97f89225d9
Viktoria Koshman <koshvikt10@gmail.com>
2026-03-26 18:40:29 +0200

    added BankAcount(hw task1)

---
+++ BankAcount.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemProgramming;
+
+public class BankAcount
+{
+    public decimal Balance { get; private set; }
+    public bool isBlocked { get; private set; } = false;
+    private static object _locker = new();
+
+    public void ShowBalance()
+    {
+        //if (isBlocked)
+        //{
+        //    Console.WriteLine("Рахунок заблокований");
+        //    return;
+        //}
+        Console.WriteLine($"Your balance: {Balance}");
+    }
+    public void AdditionAcount(decimal money)
+    {
+        while (true)
+        {
+            lock (_locker)
+            {
+                if (isBlocked == true)
+                {
+                    Console.WriteLine("Рахунок заблокований");
+                    return;
+                }
+                if (money > 0)
+                {
+                    Thread.Sleep(100);
+
+
+                    Balance += money;
+                    Console.WriteLine("Операція виконана успішно");
+                    break;
+                }
+            }
+        }
+        
+    }
+
+    public void WithdrawMoney(decimal money)
+    {
+        while (true) {
+            lock (_locker)
+            {
+                if (isBlocked == true)
+                {
+
+                    Console.WriteLine("Рахунок заблокований");
+                    return;
+                }
+
+                if (money < 0)
+                {
+                    Console.WriteLine("Число має бути додатнє");
+                    return;
+                }
+                if (money > Balance)
+                {
+                    Console.WriteLine("Недостатньо коштів");
+                    isBlocked = true;
+                    return;
+                }
+
+                Balance -= money;
+                Thread.Sleep(100);
+
+                Console.WriteLine("Операція виконана успішно");
+                break;
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,106 +1,289 @@
 ﻿using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.Threading;
+using System.Text.Json;
 namespace SystemProgramming;
 
 
+class Pizza
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public override string ToString()
+    {
+        return $"{Name} - {Price}";
+    }
+}
+
+class Document
+{
+    public string Name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name}";
+    }
+
+}
+
+class Currency
+{
+    public int r030 { get; set; }
+    public string txt { get; set; }
+    public decimal rate { get; set; }
+
+    public override string ToString()
+    {
+        return $"Currency: {txt} Rate: {rate}";
+    }
+}
 internal class Program
 {
-    static ConcurrentQueue<Order> ordersQueue = new ConcurrentQueue<Order>();
-    private static bool isWorking = true;
-    private static object _locker = new();
-    private static int total = 0;
-    static void TestThread2()
-    {
-        for (int i = 0; i <= 20; i++)
-        {
-            Thread.Sleep(500);
-            Console.WriteLine($"Thread # {Thread.CurrentThread.ManagedThreadId} \t{i}");
+    private static readonly string URL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+    //static ConcurrentQueue<Order> ordersQueue = new ConcurrentQueue<Order>();
+    //private static bool isWorking = true;
+    //private static object _locker = new();
+    //private static int total = 0;
+    //static void TestThread2()
+    //{
+    //    for (int i = 0; i <= 20; i++)
+    //    {
+    //        Thread.Sleep(500);
+    //        Console.WriteLine($"Thread # {Thread.CurrentThread.ManagedThreadId} \t{i}");
 
+    //    }
+    //}
+    //static void TestThread()
+    //{
+    //    Thread.Sleep(3000);
+    //    Console.WriteLine("Start TestThread");
+    //    Console.WriteLine($"Core Main {Thread.GetCurrentProcessorId()}");
+    //    Console.WriteLine($"Thread # {Thread.CurrentThread.ManagedThreadId}");
+
+    //    Console.WriteLine("Finish TestThread");
+    //}
+
+    //static void Up()
+    //{
+    //    const int N = 10;
+    //    for (int i = 0; i < N; i++)
+    //    {
+    //        lock(_locker)
+    //        {
+
+    //            ++total;
+    //        }
+    //        Thread.Sleep(1);
+    //    }
+    //}
+    //static void ProcessOrders()
+    //{
+    //    while (isWorking || !ordersQueue.IsEmpty)
+    //    {
+    //        if (ordersQueue.TryDequeue(out Order order))
+    //        {
+    //            Console.WriteLine(
+    //                $"Працівник {Thread.CurrentThread.ManagedThreadId} обробляє замовлення {order.Id}: {order.ProductName}"
+    //            );
+
+    //            Thread.Sleep(500);
+
+    //            Console.WriteLine(
+    //                $"Order # {order.Id} for product {order.ProductName} has been completed"
+    //            );
+    //        }
+    //    }
+    //}
+
+    //static void PrintLow()
+    //{
+    //    for (int i = 0; i <= 20; i++)
+    //    {
+    //        Console.WriteLine($"Low - {i}");
+    //        Thread.Sleep(100);
+    //    }
+    //}
+    //static void PrintNormal()
+    //{
+    //    for (int i = 0; i <= 20; i++)
+    //    {
+    //        Console.WriteLine($"Normal - {i}");
+    //        Thread.Sleep(100);
+    //    }
+    //}
+    //static void PrintHigh()
+    //{
+    //    for (int i = 0; i <= 20; i++)
+    //    {
+    //        Console.WriteLine($"High - {i}");
+    //        Thread.Sleep(100);
+    //    }
+    //}
+
+
+    static async Task<Pizza> DoPizza()
+    {
+        Console.WriteLine("Отримали замовлення");
+        Console.WriteLine("Починвємо готувати");
+        await Task.Delay(500);
+        Console.WriteLine("Готуємо тісто");
+        await Task.Delay(1000);
+        Console.WriteLine("Готуємо начинку");
+        await Task.Delay(1000);
+        Console.WriteLine("Випікаємо піцу");
+        await Task.Delay(1000);
+        Console.WriteLine("Все готово");
+        return new Pizza { Name = "Margarita", Price = 300 };
+
+    }
+
+    static async Task<List<Document>> UploadingDocuments()
+    {
+        List<Document> docs = new List<Document>();
+        Console.WriteLine("Початок завантаження документів");
+
+        for (int i = 1; i <= 3; i++)
+        {
+            await Task.Delay(1000);
+            Console.WriteLine($"Завантажуємо документ {i}");
+
+            await Task.Delay(1000);
+            Console.WriteLine($"Заповнюємо дані документу {i}");
+
+            docs.Add(new Document { Name = $"Документ {i}" });
         }
-    }
-    static void TestThread()
-    {
-        Thread.Sleep(3000);
-        Console.WriteLine("Start TestThread");
-        Console.WriteLine($"Core Main {Thread.GetCurrentProcessorId()}");
-        Console.WriteLine($"Thread # {Thread.CurrentThread.ManagedThreadId}");
 
-        Console.WriteLine("Finish TestThread");
+        Console.WriteLine("Все готово");
+        return docs;
     }
 
-    static void Up()
-    {
-        const int N = 10;
-        for (int i = 0; i < N; i++)
+    static async Task<List<Currency>> GetCurrency(){
+
+        using (HttpClient client = new HttpClient())
         {
-            lock(_locker)
+            try
             {
-
-                ++total;
+                var response = await client.GetStringAsync(URL);
+                var obj = JsonSerializer.Deserialize<List<Currency>>(response);
+                if(obj != null)
+                {
+                    return obj;
+                }
             }
-            Thread.Sleep(1);
-        }
-    }
-    static void ProcessOrders()
-    {
-        while (isWorking || !ordersQueue.IsEmpty)
-        {
-            if (ordersQueue.TryDequeue(out Order order))
+            catch(Exception ex)
             {
-                Console.WriteLine(
-                    $"Працівник {Thread.CurrentThread.ManagedThreadId} обробляє замовлення {order.Id}: {order.ProductName}"
-                );
-
-                Thread.Sleep(500);
-
-                Console.WriteLine(
-                    $"Order # {order.Id} for product {order.ProductName} has been completed"
-                );
+                Console.WriteLine(ex.Message);
             }
+            return null;
+
         }
+
+
     }
 
-    static void PrintLow()
+
+    static decimal ConvertToDolars(decimal grn, List<Currency> currencies)
     {
-        for (int i = 0; i <= 20; i++)
+        var dollar = currencies.FirstOrDefault(d => d.txt == "Долар США");
+        return grn / dollar.rate;
+    }
+    static async Task Main(string[] args)
+    {
+        BankAcount bankAcount = new BankAcount();
+        Thread thread1 = new Thread(_ =>
         {
-            Console.WriteLine($"Low - {i}");
-            Thread.Sleep(100);
-        }
-    }
-    static void PrintNormal()
-    {
-        for (int i = 0; i <= 20; i++)
+            bankAcount.AdditionAcount(100);
+        });
+        Thread thread2 = new Thread(_ =>
         {
-            Console.WriteLine($"Normal - {i}");
-            Thread.Sleep(100);
-        }
-    }
-    static void PrintHigh()
-    {
-        for (int i = 0; i <= 20; i++)
+            bankAcount.AdditionAcount(200);
+        });
+        Thread thread3 = new Thread(_ =>
         {
-            Console.WriteLine($"High - {i}");
-            Thread.Sleep(100);
-        }
-    }
-    static void Main(string[] args)
-    {
-        Thread threadLow = new Thread(PrintLow);
-        Thread threadNormal = new Thread(PrintNormal);
-        Thread threadHigh = new Thread(PrintHigh);
+            bankAcount.AdditionAcount(150);
+        });
 
-        threadLow.Priority = ThreadPriority.Lowest;
-        threadNormal.Priority = ThreadPriority.Normal;
-        threadHigh.Priority = ThreadPriority.Highest;
 
-        threadLow.Start();
-        threadNormal.Start();
-        threadHigh.Start();
 
-        threadLow.Join();
-        threadNormal.Join();
-        threadHigh.Join();
+        Thread thread4 = new Thread(_ =>
+        {
+            bankAcount.WithdrawMoney(400);
+        });
+        Thread thread5 = new Thread(_ =>
+        {
+            bankAcount.WithdrawMoney(400);
+        });
+        Thread thread6 = new Thread(_ =>
+        {
+            bankAcount.WithdrawMoney(400);
+        });
+
+        bankAcount.ShowBalance();
+        thread1.Start();
+        thread2.Start();
+        thread3.Start();
+
+        thread1.Join();
+        thread2.Join();
+        thread3.Join();
+        bankAcount.ShowBalance();
+
+        thread4.Start();
+        thread5.Start();
+        thread6.Start();
+
+        thread4.Join();
+        thread5.Join();
+        thread6.Join();
+
+        bankAcount.ShowBalance();
+
+        //Console.WriteLine("Введіть скільки грн: ");
+        //decimal grn = Convert.ToDecimal(Console.ReadLine());
+
+
+        //var data = await GetCurrency();
+        //if (data != null)
+        //{
+        //    //foreach (var item in data)
+        //    //{
+        //    //    Console.WriteLine(item);
+        //    //}
+        //    var result = ConvertToDolars(grn, data);
+        //    Console.WriteLine($"Приблизно: {result} USD");
+        //}
+
+
+
+        //var documents = UploadingDocuments();
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    Console.WriteLine("Ми працюємо...");
+        //    Thread.Sleep(500);
+        //}
+        //documents.Wait();
+        //Console.WriteLine("Отримали документи");
+        //foreach (var item in documents.Result)
+        //{
+        //    Console.WriteLine(item);
+        //}
+
+
+        //Thread threadLow = new Thread(PrintLow);
+        //Thread threadNormal = new Thread(PrintNormal);
+        //Thread threadHigh = new Thread(PrintHigh);
+
+        //threadLow.Priority = ThreadPriority.Lowest;
+        //threadNormal.Priority = ThreadPriority.Normal;
+        //threadHigh.Priority = ThreadPriority.Highest;
+
+        //threadLow.Start();
+        //threadNormal.Start();
+        //threadHigh.Start();
+
+        //threadLow.Join();
+        //threadNormal.Join();
+        //threadHigh.Join();
 
         //const int N = 3;
         //CountdownEvent done = new(N);
